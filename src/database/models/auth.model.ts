e5b10c2660500c57f0485b803f941b332977b2a3
Mahmoud <sragmahmoud4@gmail.com>
2023-03-17 10:23:34 -0400

    fix: :bug: fix verify-email api
    PR Close #UNKP-3

--- src/database/models/auth.model.ts
+++ src/database/models/auth.model.ts
@@ -11,11 +11,18 @@ export class AuthModel {
     return await this.authModel.findOne({ email }).select('_id').lean();
   }
 
-  async createNewUser(email: string, otpCode: number) {
-    await this.authModel.create({ email, otpCode, otpCreatedAt: Date.now() });
+  async createNewUser(
+    email: string,
+    otpCode: number,
+  ): Promise<{ _id: string }> {
+    return await this.authModel.create({
+      email,
+      otpCode,
+      otpCreatedAt: Date.now(),
+    });
   }
 
-  async updateUserOTP(email: string, otpCode: number) {
+  async updateUserOTP(email: string, otpCode: number): Promise<void> {
     const otpCreatedAt = new Date(Date.now());
     await this.authModel.findOneAndUpdate({ email }, { otpCode, otpCreatedAt });
   }
